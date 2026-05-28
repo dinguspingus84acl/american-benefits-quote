b84b8c7527de824f47b0fa7d171efc1bef1cd073
dinguspingus84acl <jack@atcostleads.com>
2026-05-27 23:58:16 -0400

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Apps Script Web App URL that receives lead submissions.
+// See SHEETS.md for how this was generated. If you redeploy a new
+// version in Apps Script Manage Deployments, the URL stays the same
+// (Apps Script versions a single deployment in place).
+export const LEADS_WEBHOOK_URL =
+  "https://script.google.com/macros/s/AKfycbyauR6sVyuu8B5FJ-iPkKSzpNJiuVVjVic1EPbXznBTeSXPaqXcYZqiBnuyFXzmveDMMw/exec";
